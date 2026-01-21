de679dd9dba595696817e361c446c57dd43a0547
ASHADDA07 <srihanujakaveeti@gmail.com>
2026-01-21 13:18:13 +0530

    Configured Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/jai-sethu-frontend/",  // 👈 must match repo name exactly
+});
